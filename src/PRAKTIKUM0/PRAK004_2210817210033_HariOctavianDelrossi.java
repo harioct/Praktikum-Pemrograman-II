d5ece9646b1287c3c4b8ca6ff39bd6a64acea8eb
harioct <octavianhari@gmail.com>
2023-10-19 02:08:37 +0800

    PRAK0

---
+++ src/PRAKTIKUM0/PRAK004_2210817210033_HariOctavianDelrossi.java
@@ -0,0 +1,10 @@
+package PRAKTIKUM0;
+
+public class PRAK004_2210817210033_HariOctavianDelrossi {
+    public static void main(String[] args){
+        String nama = "Hari Octavian Delrossi";
+        int umur = 19;
+
+        System.out.println("Perkenalkan, Nama Saya " + nama +", Umur Saya " + umur);
+    }
+}
