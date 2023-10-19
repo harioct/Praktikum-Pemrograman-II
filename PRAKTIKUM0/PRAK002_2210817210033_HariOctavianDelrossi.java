d32a07a255dcfb8c7c5f37edae54e7bdd417e7c4
harioct <114865767+harioct@users.noreply.github.com>
2023-10-19 22:12:54 +0800

    Add files via upload

---
+++ PRAKTIKUM0/PRAK002_2210817210033_HariOctavianDelrossi.java
@@ -0,0 +1,12 @@
+package PRAKTIKUM0;
+
+public class PRAK002_2210817210033_HariOctavianDelrossi {
+    public static void main(String[] args){
+        System.out.println("##############################");
+        System.out.println("#                            #");
+        System.out.println("#   Hari Octavian Delrossi   #");
+        System.out.println("#       2210817210033        #");
+        System.out.println("#                            #");
+        System.out.println("##############################");
+    }
+}
